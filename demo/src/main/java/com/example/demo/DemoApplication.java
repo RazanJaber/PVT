7b4760f1ffd518692fbdb021794ddc10c77d82f7
RazanJaber <razan.jabir@gmail.com>
2019-04-29 14:26:44 +0200

    First commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,6 @@ public class DemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { return builder.sources(DemoApplication.class);
--- demo/src/main/java/com/example/demo/HelloWorldControler.java
+++ demo/src/main/java/com/example/demo/HelloWorldControler.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldControler {
-	 @GetMapping("/hello")
-	    public String sayHello() {
-	        return "Hello Java!";
-	    }	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello Sweden!";
+	}
+
+	@RequestMapping(value = "/sayHello/{name}", method = RequestMethod.GET)
+	public String greeting(@PathVariable String name) {
+		// String name now has the value from the url return String.format(“Hello, %s.”,
+		// name);
+		
+		return String.format("Hello, %s.", name);
+	}
 }
