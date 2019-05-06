db1c5fcce2e010be94efba5ddb9600d6d605b842
RazanJaber <razan.jabir@gmail.com>
2019-05-06 16:14:45 +0200

    commit

--- demo/src/main/java/com/example/demo/HelloWorldControler.java
+++ demo/src/main/java/com/example/demo/HelloWorldControler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldControler {
+	
+	
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello Sweden!";
